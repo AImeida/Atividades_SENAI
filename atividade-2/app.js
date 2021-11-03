ed92a8a38d6a17e5f741b142a89fd06d028ff1cb
Gabriel Almeida <williamgabrielg@gmail.com>
2021-11-03 18:56:01 -0300

    updates

--- atividade-2/app.js
+++ atividade-2/app.js
@@ -1,3 +1,24 @@
-var modelo = document.getElementById('modelo')
-var preço = document.getElementById('preço')
+form.addEventListener('submit', function(){
+    event.preventDefault()
 
+    var modelo = document.getElementById('modelo').value
+    console.log(modelo)
+
+    var preço = document.getElementById('preço').value
+    preço = Number(preço)
+    console.log(preço)
+
+    var parcelas = .5*preço / 60
+    console.log(parcelas)
+
+    var parcelas_juros = parcelas + 0.7*parcelas
+    parcelas_juros = parcelas_juros.toPrecision(6)
+    console.log(parcelas_juros)
+
+
+    document.getElementById('mostrar').innerHTML = `
+        <h2>Promoção: ${modelo}</h2>
+        <h2>Entrada de ${.5*preço}</h2>
+        <h2>+60 de R$: ${parcelas_juros}</h2>
+    `
+})
